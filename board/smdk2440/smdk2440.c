481f3168a20c8e4278664ee66366b77e84109388
xieshaohua <shaohua.xie@pactera.com>
2017-07-14 11:05:56 +0800

    delete lowlevel_init.S

--- board/smdk2440/smdk2440.c
+++ board/smdk2440/smdk2440.c
@@ -89,10 +89,7 @@ int dram_init(void)
 #ifdef CONFIG_CMD_NET
 int board_eth_init(bd_t *bis)
 {
-	int rc = 0;
-#ifdef CONFIG_CS8900
-	rc = cs8900_initialize(0, CONFIG_CS8900_BASE);
-#endif
+	int rc = -1;
 #ifdef CONFIG_DRIVER_DM9000
 	rc = dm9000_initialize(bis);
 #endif
